2db6f35ded628a7a18b7dad1d375d39d57e1586a
AlexSwiss <alexyikeh@gmail.com>
2022-01-09 16:58:07 +0100

    added wootlab project as submodule

--- ryda-app/src/utils/successResponse.js
+++
@@ -1,9 +0,0 @@
-const successResponse = async ( res, statusCode, message, data = {}) => {
-    res.status(statusCode).json({
-      status: 'success',
-      message,
-      data
-    });
-  };
-  
-  module.exports = successResponse;
